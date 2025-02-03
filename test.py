6252fe6095aa59aad60cf0bcb037fc358bdc787c
Logan White <135063791+LWhite49@users.noreply.github.com>
2025-02-03 00:59:47 -0500

    Trained, Saved, and Loaded Parameters for Linaer Regression model

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
+from pathlib import Path
+
 
 # Set seed
 torch.manual_seed(12)
@@ -19,6 +21,9 @@ class LinearRegression(nn.Module):
     
     def log_state(self):
         print(f"Model state: {self.state_dict()}")
+
+    def save_params(self, path: Path):
+        torch.save(obj=self.state_dict(), f=path)
     
 
 # Source input data
@@ -40,69 +45,88 @@ y_test = y_dataset[partition:]
 
 model_0 = LinearRegression()
 
-loss_fn = nn.L1Loss()
+# loss_fn = nn.L1Loss()
 
-optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+# optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
 
-# Create training loop
+# # Create training loop
 
-epochs = 300
+# epochs = 300
 
-train_losses = []
-test_losses = []
-epoch_count = []
+# train_losses = []
+# test_losses = []
+# epoch_count = []
 
-for epoch in range(epochs):
-    # Put model in training mode
-    model_0.train()
+# for epoch in range(epochs):
+#     # Put model in training mode
+#     model_0.train()
 
-    # Forward Pass with training data
-    y_pred = model_0(x_train)
+#     # Forward Pass with training data
+#     y_pred = model_0(x_train)
 
-    # Calculate Loss
-    loss = loss_fn(y_pred, y_train)
+#     # Calculate Loss
+#     loss = loss_fn(y_pred, y_train)
 
-    # Take Zero Gradient
-    optimizer.zero_grad()
+#     # Take Zero Gradient
+#     optimizer.zero_grad()
 
-    # Backwards Pass
-    loss.backward()
+#     # Backwards Pass
+#     loss.backward()
 
-    # Update Parameters
-    optimizer.step()
+#     # Update Parameters
+#     optimizer.step()
 
-    # Put model in evaluation mode
-    model_0.eval()
+#     # Put model in evaluation mode
+#     model_0.eval()
 
-    with torch.inference_mode():
-        # Forward Pass test data
-        test_pred = model_0(x_test)
+#     with torch.inference_mode():
+#         # Forward Pass test data
+#         test_pred = model_0(x_test)
 
-        # Calculate loss from test data
-        test_loss = loss_fn(test_pred, y_test.type(torch.float))
+#         # Calculate loss from test data
+#         test_loss = loss_fn(test_pred, y_test.type(torch.float))
 
-        # Log training state
-        if epoch % 10 == 0:
-            epoch_count.append(epoch)
-            train_losses.append(loss.detach().numpy())
-            test_losses.append(test_loss.detach().numpy())
-            print(f"Epoch: {epoch}")
-            model_0.log_state()
+#         # Log training state
+#         if epoch % 10 == 0:
+#             epoch_count.append(epoch)
+#             train_losses.append(loss.detach().numpy())
+#             test_losses.append(test_loss.detach().numpy())
+#             print(f"Epoch: {epoch}")
+#             model_0.log_state()
 
-# Chart data with Matplotlib
-print("Train Losses: ", train_losses)
-print("Test Losses: ", test_losses)
-plt.plot(epoch_count, train_losses, label="Train Loss")
-plt.plot(epoch_count, test_losses, label="Test Loss")
-plt.title("Loss over Epochs")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.legend()
-plt.show()
+# # Chart data with Matplotlib
+# print("Train Losses: ", train_losses)
+# print("Test Losses: ", test_losses)
+# plt.plot(epoch_count, train_losses, label="Train Loss")
+# plt.plot(epoch_count, test_losses, label="Test Loss")
+# plt.title("Loss over Epochs")
+# plt.xlabel("Epochs")
+# plt.ylabel("Loss")
+# plt.legend()
+# plt.show()
 
 model_0.log_state()
 
-# Make Predictions
+# # Make Predictions
+# model_0.eval()
+# with torch.inference_mode():
+#     y_pred = model_0(x_dataset)
+#     plt.plot(x_dataset, y_dataset, label="Actual")
+#     plt.plot(x_dataset, y_pred.detach().numpy(), label="Predicted", color='red')
+#     plt.title("Actual vs Predicted")
+#     plt.legend()
+#     plt.show()
+
+# Save Params
+# temp_path = Path("models")
+# temp_path.mkdir(parents=True, exist_ok=True)
+# model_name = "model_0.pth"
+# model_0.save_params(temp_path / model_name)
+
+# Load Params
+temp_path = Path("models")
+model_name = "model_0.pth"
+model_0.load_state_dict(torch.load(temp_path / model_name))
 model_0.eval()
 with torch.inference_mode():
     y_pred = model_0(x_dataset)
@@ -111,6 +135,8 @@ with torch.inference_mode():
     plt.title("Actual vs Predicted")
     plt.legend()
     plt.show()
+
+
 
 
 
